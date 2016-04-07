e8788d6039e6f8b24188a9401cd1782d37bc6155
user <user@localhost.localdomain>
2016-04-07 13:42:03 -0500

    first commit

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+
+public class Calculator{
+  
+  public static void main(String in[])throws FileNotFoundException{
+  
+    Parser test= new Parser();
+  
+  }
+}
---
+++ NonTerminal.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public abstract class NonTerminal{
+  Queue children= new LinkedList();
+  List<String> string_list= new ArrayList<>();
+
+}
